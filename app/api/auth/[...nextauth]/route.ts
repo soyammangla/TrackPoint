550b7ad8cbdf18735a283ab3d2c0bf4dcc060442
soyammangla <soyammangla15@gmail.com>
2025-12-26 12:36:56 +0530

    msg

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,21 +1,27 @@
-import { authOptions } from "@/lib/authoptions";
-import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { NextAuthOptions } from "next-auth";
 
-const handler = NextAuth(authOptions);
-
-export { handler as GET, handler as POST };
-
-// import NextAuth from "next-auth";
-// import GoogleProvider from "next-auth/providers/google";
-
-// const handler = NextAuth({
-//   providers: [
-//     GoogleProvider({
-//       clientId: process.env.GOOGLE_CLIENT_ID!,
-//       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-//     }),
-//   ],
-//   secret: process.env.NEXTAUTH_SECRET,
-// });
-
-// export { handler as GET, handler as POST };
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    // Add other providers if needed
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  cookies: {
+    sessionToken: {
+      name: `__Secure-next-auth.session-token`,
+      options: {
+        httpOnly: true,
+        sameSite: "lax",
+        path: "/",
+        secure: process.env.NODE_ENV === "production", // localhost pe false, deploy pe true
+      },
+    },
+  },
+  session: {
+    strategy: "jwt", // ya database session strategy
+  },
+};
