1fc39117dbc6a3fdcb4aff0e541d0076905c0b88
soyammangla <soyammangla15@gmail.com>
2026-01-03 16:43:32 +0530

    fix error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "next-themes"; // ✅ add this
+import { ThemeProvider } from "next-themes";
 import Provider from "@/components/provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
   subsets: ["latin"],
 });
 
@@ -24,17 +19,15 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} antialiased`}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="light" // <-- yahi important hai
+          defaultTheme="light"
           enableSystem={false}
         >
           <Provider>{children}</Provider>
