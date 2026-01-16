29197afb86579128298a5cb6536fcfc20b20b550
soyammangla <soyammangla15@gmail.com>
2026-01-16 14:20:04 +0530

    chngs in mext.config.ts remove warnings add some image patterns

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,45 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   images: {
+//     domains: [
+//       "lh3.googleusercontent.com",
+//       "example.com",
+//       "i.pinimg.com",
+//       "m.media-amazon.com",
+//     ],
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "in.pinterest.com",
+//       },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: [
-      "lh3.googleusercontent.com",
-      "example.com",
-      "i.pinimg.com",
-      "m.media-amazon.com",
-    ],
     remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "example.com",
+      },
+      {
+        protocol: "https",
+        hostname: "i.pinimg.com",
+      },
+      {
+        protocol: "https",
+        hostname: "m.media-amazon.com",
+      },
       {
         protocol: "https",
         hostname: "in.pinterest.com",
